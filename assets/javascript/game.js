245224cd7ad6c681705cf307101e48feb8d4df7d
Tyler Ward <T.ward26@gmail.com>
2019-03-04 18:28:16 -0600

    Initial game functionality implemented

---
+++ assets/javascript/game.js
@@ -0,0 +1,88 @@
+//Variable declaration and assignment
+
+//variable for possible word choices for game
+var wordChoiceList = ["TEST", "CENTER", "WORKING", "LAVA", "ASSEMBLAGE", "OHGODWHY"]
+//variable for randomly selected word
+var computerChoice = wordChoiceList[Math.floor(Math.random() * wordChoiceList.length)];
+//variables to hold player letter guesses
+var rightGuess = [];
+var correctString = "";
+var userGuess;
+var wrongGuesses = [];
+//regex variable for making sure only alpha characters are pressed
+var alphaExp = /^[a-zA-Z]+$/;
+//starting point variables
+var guessCount = 12;
+var winCount = 0;
+//variables to replace html spans
+var winsText = document.getElementById("wins-text");
+var currentWordText = document.getElementById("current-word");
+var guessesLeftText = document.getElementById("guesses-left");
+var lettersGuessedText = document.getElementById("letters-guessed");
+
+//Functions
+function resetGame() {
+    computerChoice = wordChoiceList[Math.floor(Math.random() * wordChoiceList.length)];
+    resetCurrentWord();
+    currentWordText.textContent = correctString;
+    wrongGuesses = [];
+    lettersGuessedText.textContent = wrongGuesses;
+    guessCount = 12;
+    guessesLeftText.textContent = guessCount;
+}
+
+function resetCurrentWord() {
+    rightGuesses = [];
+    for (var i = 0; i < computerChoice.length; i++) {
+        rightGuesses.push('-');
+        correctString = rightGuesses.join("");
+    }
+}
+
+//initial page display info
+guessesLeftText.textContent = guessCount;
+resetCurrentWord();
+currentWordText.textContent = correctString;
+
+//Game itself
+document.onkeyup = function (event) {
+
+    // Determines which key was pressed.
+    userGuess = event.key.toUpperCase();
+
+    //checks to make sure key pressed was a letter
+    if(userGuess.match(alphaExp)) {
+        //if correct letter guessed find all instances of it and replace - with letter
+        if (computerChoice.indexOf(userGuess) !== -1) {
+            for (var j = 0; j < computerChoice.length; j++) {
+                if (computerChoice[j] == userGuess) {
+                    rightGuesses[j] = computerChoice[j];
+                    correctString = rightGuesses.join("");
+                    currentWordText.textContent = correctString;
+                }
+            }
+        }
+        //if wrong letter guessed and letter hasn't already been guessed
+        else if ((computerChoice.indexOf(userGuess) === -1) && (wrongGuesses.indexOf(userGuess) === -1)) {
+            guessCount--;
+            guessesLeftText.textContent = guessCount;
+            wrongGuesses.push(userGuess);
+            lettersGuessedText.textContent = wrongGuesses;
+        }
+        //checks win condition
+        if (computerChoice === correctString) {
+            winCount++;
+            winsText.textContent = winCount;
+            resetGame();
+        }
+        //checks loss condition
+        if (guessCount === 0) {
+            resetGame();
+        }
+
+    }
+    //if letter key wasn't pressed pops up alert
+    else {
+        alert("Please enter only letters");
+    }
+};
